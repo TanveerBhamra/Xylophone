26109f26009d7c50f4c7ef1ee3e1bd11c220b525
TanveerBhamra <81157724+TanveerBhamra@users.noreply.github.com>
2021-05-19 20:48:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var music;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = "blue";
+
+    block2 = createSprite(295,580,200,30);
+    block2.shapeColor = "orange";
+
+    block3 = createSprite(510,580,200,30);
+    block3.shapeColor = "red";
+
+    block4 = createSprite(720,580,200,30);
+    block4.shapeColor = "green";
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = "white";
+    ball.velocityX = random(5,10);
+    ball.velocityY = random(5,10);
+
+}
+
+function draw(){
+    background(rgb(169,169,169));
+    edges=createEdgeSprites();
+   
+    if(block1.isTouching(ball) && ball.bounceOff(block1)){
+        ball.shapeColor = "blue";
+        music.play();
+    }
+
+    if(block2.isTouching(ball) && ball.bounceOff(block2)){
+        ball.shapeColor = "orange";
+        music.play();
+    }
+
+    if(block3.isTouching(ball) && ball.bounceOff(block3)){
+        ball.shapeColor = "red";
+        music.play();
+    }
+
+    if(block4.isTouching(ball) && ball.bounceOff(block4)){
+        ball.shapeColor = "green";
+        music.play();
+    }
+
+    ball.bounceOff(edges);
+    ball.bounceOff(block1);
+    ball.bounceOff(block2);
+    ball.bounceOff(block3);
+    ball.bounceOff(block4);
+
+    drawSprites();
+}
